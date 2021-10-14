176eded4fefec7cb6cc6fe0cb9474268a4c188eb
dylanchanghuang <asy167@gmail.com>
2021-10-14 18:16:57 +0800

    使用Entity Framework產生API Controller的CRUD功能

--- Northwind.API/Startup.cs
+++ Northwind.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Northwind.Domain.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,10 @@ namespace Northwind.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<NorthwindContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
